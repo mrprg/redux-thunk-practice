341cd9be72a4329136d093d9a59ac937d8ff6ec8
mrprg <mohamadrezaasadi322@gmail.com>
2023-01-03 23:54:38 +0330

    index file

--- reduxapp/src/index.js
+++ reduxapp/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+import App from './components/App';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={createStore(reducers)}>
+      <App/>
+    </Provider>
   </React.StrictMode>
 );
 
