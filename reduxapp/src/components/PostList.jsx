83b05fb0ad1e60eb97f2c6ccd6535751cf99ff74
mrprg <mohamadrezaasadi322@gmail.com>
2023-01-04 21:55:36 +0330

    writing up an action creator

--- reduxapp/src/components/PostList.jsx
+++ reduxapp/src/components/PostList.jsx
@@ -1,9 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
-export default class PostList extends Component {
+class PostList extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+  
   render() {
     return (
       <div>PostList</div>
     )
   }
-}
+};
+
+export default connect(null, { fetchPosts })(PostList);
