5269ef7138d9c8c11b6e25db6bba3e6a3ccc48a8
mrprg <mohamadrezaasadi322@gmail.com>
2023-01-04 23:23:04 +0330

    hook up middleware to redux store

--- reduxapp/src/index.js
+++ reduxapp/src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore , applyMiddleware} from 'redux';
 import reducers from './reducers';
 import App from './components/App';
+import thunk from 'redux-thunk';
 
+const store = createStore(reducers, applyMiddleware(thunk));
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={createStore(reducers)}>
+    <Provider store={store}>
       <App/>
     </Provider>
   </React.StrictMode>
